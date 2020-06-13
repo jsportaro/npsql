b241dfb67ed1e73af448acb4ad38d0a05204ba3b
Joe Portaro <jsportaro@outlook.com>
2020-06-13 10:36:13 -0400

    Moving eval to its own CU.

--- server/include/expr_eval.h
+++ server/include/expr_eval.h
@@ -1,11 +1,12 @@
 #ifndef __EXPR_EVAL_H__
 #define __EXPR_EVAL_H__
 
+#include <scans.h>
+#include <sql.h>
+#include <value.h>
+
 #include <stdbool.h>
 
-struct eval_result
-{
-    bool error;
-};
+struct value eval(struct expr_ctx *ctx, struct scan *sscan);
 
 #endif
--- server/src/expr_eval.c
+++ server/src/expr_eval.c
@@ -1,66 +1,93 @@
+#include <common.h>
 #include <expr_eval.h>
+#include <value.h>
 
-struct scan_field
-eval(struct expr *expr)
+struct value eval(struct expr_ctx *ctx, struct scan *scan)
 {
+    struct value v = { 0 };
+    UNUSED(ctx);
+    UNUSED(scan);
+
     struct term_expr *term;
-    struct infix_expr *infix = NULL;
+    //struct infix_expr *infix = NULL;
     
-    struct scan_field r;
-    struct scan_field lr, rr;
+    // struct eval_result r;
+    // struct eval_result lr, rr;
 
-    r.type = EXPR_INTEGER;
-    r.value.number = 0;
-    r.error = false;
-    switch(expr->type)
+    switch(ctx->expr->type)
     {
-        case EXPR_INTEGER:
-            term = (struct term_expr *)expr;
-            r.value.number = term->value.number;
-            r.type = EXPR_INTEGER;
-            return r;
         case EXPR_IDENIFIER:
-        case EXPR_STRING:
-            term = (struct term_expr *)expr;
-            r.value.s = term->value.string;
-            r.type = EXPR_IDENIFIER;
-            return r;
-        case EXPR_ADD:
-        case EXPR_SUB:
-        case EXPR_MUL:
-        case EXPR_DIV:
-        case EXPR_COMPARISON:
-            infix = (struct infix_expr *)expr;
-            lr = eval(infix->l);
-            rr = eval(infix->r);
-           
-            // Type saftey!  Also, only ints for now
-            if (lr.type != rr.type && lr.type == EXPR_INTEGER)
-            {
-                r.error = true;
-                goto end;
-            }
-
-            switch (infix->type)
-            {
-                case EXPR_ADD:
-                    r.value.number = lr.value.number + rr.value.number;
-                    break;
-                case EXPR_SUB:
-                    r.value.number = lr.value.number - rr.value.number;
-                    break;
-                case EXPR_MUL:
-                    r.value.number = lr.value.number * rr.value.number;
-                    break;
-                case EXPR_DIV:
-                    r.value.number = lr.value.number / rr.value.number;
-                    break;
-                default:
-                    break;
-            }
-
+            term = (struct term_expr *)ctx->expr;
+            scan->get_value(scan, term->value.string, &v);
+        default:
             break;
     }
-end:
-    return r;
+
+    return v;
 }
+
+// struct scan_field
+// eval(struct expr *expr)
+// {
+//     struct term_expr *term;
+//     struct infix_expr *infix = NULL;
+    
+//     struct scan_field r;
+//     struct scan_field lr, rr;
+
+//     r.type = EXPR_INTEGER;
+//     r.value.number = 0;
+//     r.error = false;
+//     switch(expr->type)
+//     {
+//         case EXPR_INTEGER:
+//             term = (struct term_expr *)expr;
+//             r.value.number = term->value.number;
+//             r.type = EXPR_INTEGER;
+//             return r;
+//         case EXPR_IDENIFIER:
+//         case EXPR_STRING:
+//             term = (struct term_expr *)expr;
+//             r.value.s = term->value.string;
+//             r.type = EXPR_IDENIFIER;
+//             return r;
+//         case EXPR_ADD:
+//         case EXPR_SUB:
+//         case EXPR_MUL:
+//         case EXPR_DIV:
+//         case EXPR_COMPARISON:
+//             infix = (struct infix_expr *)expr;
+//             lr = eval(infix->l);
+//             rr = eval(infix->r);
+           
+//             // Type saftey!  Also, only ints for now
+//             if (lr.type != rr.type && lr.type == EXPR_INTEGER)
+//             {
+//                 r.error = true;
+//                 goto end;
+//             }
+
+//             switch (infix->type)
+//             {
+//                 case EXPR_ADD:
+//                     r.value.number = lr.value.number + rr.value.number;
+//                     break;
+//                 case EXPR_SUB:
+//                     r.value.number = lr.value.number - rr.value.number;
+//                     break;
+//                 case EXPR_MUL:
+//                     r.value.number = lr.value.number * rr.value.number;
+//                     break;
+//                 case EXPR_DIV:
+//                     r.value.number = lr.value.number / rr.value.number;
+//                     break;
+//                 default:
+//                     break;
+//             }
+
+//             break;
+//     }
+// end:
+//     return r;
+// }
+
