dcfb65b41afe6824a192b8cddc4595a7ec26fe9e
Joe Portaro <jsportaro@outlook.com>
2020-06-15 21:23:14 -0400

    Removing dead code in scan.c

--- server/src/scans.c
+++ server/src/scans.c
@@ -7,12 +7,6 @@
 #include <scans.h>
 #include <storage.h>
 
-struct resolved_value
-{
-    struct value v;
-    char *name;
-};
-
 static bool 
 select_scan_next(struct scan *scan);
 
@@ -25,100 +19,6 @@ table_scan_next(struct scan *scan);
 static void
 table_scan_get_value(struct scan *scan, char *name, struct value *v);
 
-void
-resolve(vector_type(struct resolved_value) values, char *name, struct value *v)
-{
-    for (size_t i = 0; i < vector_size(values); i++)
-    {
-        if (strncmp(name, values[i].name, MAX_COLUMN_NAME) == 0)
-        {
-            *v = values[i].v;
-        }
-    }
-
-}
-
-struct value
-eval_expr(struct expr *expr, vector_type(struct resolved_value) resolved)
-{
-    struct term_expr *term;
-    struct value v;
-    switch(expr->type)
-    {
-        case EXPR_IDENIFIER:
-            term = (struct term_expr *)expr;
-            resolve(resolved, term->value.string, &v);
-
-            return v;
-        default:
-            exit(EXIT_FAILURE);
-    }
-}
-
-// struct scan_field
-// eval(struct expr *expr)
-// {
-//     struct term_expr *term;
-//     struct infix_expr *infix = NULL;
-    
-//     struct scan_field r;
-//     struct scan_field lr, rr;
-
-//     r.type = EXPR_INTEGER;
-//     r.value.number = 0;
-//     r.error = false;
-//     switch(expr->type)
-//     {
-//         case EXPR_INTEGER:
-//             term = (struct term_expr *)expr;
-//             r.value.number = term->value.number;
-//             r.type = EXPR_INTEGER;
-//             return r;
-//         case EXPR_IDENIFIER:
-//         case EXPR_STRING:
-//             term = (struct term_expr *)expr;
-//             r.value.s = term->value.string;
-//             r.type = EXPR_IDENIFIER;
-//             return r;
-//         case EXPR_ADD:
-//         case EXPR_SUB:
-//         case EXPR_MUL:
-//         case EXPR_DIV:
-//         case EXPR_COMPARISON:
-//             infix = (struct infix_expr *)expr;
-//             lr = eval(infix->l);
-//             rr = eval(infix->r);
-           
-//             // Type saftey!  Also, only ints for now
-//             if (lr.type != rr.type && lr.type == EXPR_INTEGER)
-//             {
-//                 r.error = true;
-//                 goto end;
-//             }
-
-//             switch (infix->type)
-//             {
-//                 case EXPR_ADD:
-//                     r.value.number = lr.value.number + rr.value.number;
-//                     break;
-//                 case EXPR_SUB:
-//                     r.value.number = lr.value.number - rr.value.number;
-//                     break;
-//                 case EXPR_MUL:
-//                     r.value.number = lr.value.number * rr.value.number;
-//                     break;
-//                 case EXPR_DIV:
-//                     r.value.number = lr.value.number / rr.value.number;
-//                     break;
-//                 default:
-//                     break;
-//             }
-
-//             break;
-//     }
-// end:
-//     return r;
-// }
 
 static bool 
 scan_project_next(struct scan *scan)
